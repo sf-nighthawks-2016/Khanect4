930ad6611d710935ebe3ba9a558f57d64fe16218
Chris Wong & Jonathan Hall & Mikey Moore <pair+chris-wong-1.jlhall.mikeymoore@devbootcamp.com>
2016-06-26 11:46:32 -0700

    Loop through current column

--- app.js
+++ app.js
@@ -1,5 +1,15 @@
 $(document).ready(function() {
   turnCounter();
+  whoseTurn();
+
+  $('input[type="submit"]').on('click',function(){
+    div = $(this).closest("div")
+    cells = div.children('.cell')
+     for (i=5;i>-1;i--) {
+      console.log(cells[i])
+    }
+
+  })
 
 
 });
@@ -9,12 +19,23 @@ function turnCounter() {
   $('form').on("submit", function(event){
       event.preventDefault();
       counter +=1;
-      console.log(counter);
   })
+  return counter;
+}
+
+function whoseTurn() {
+  if (turnCounter() % 2 == 0) {
+    return "black";
+  }
+  else {
+    return "red";
+  }
 }
 
 
 
+
+// $(dfdk).prepend("<img alt='ghengis' src='kdjfk.jpg'>")
 
 
 
