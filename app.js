e8b4e4cd155cf29bf58658fad4c935d2e3cdad93
Chris Wong & Jonathan Hall & Mikey Moore <pair+chris-wong-1.jlhall.mikeymoore@devbootcamp.com>
2016-06-26 12:36:10 -0700

    Can insert into cells through loop

--- app.js
+++ app.js
@@ -3,12 +3,20 @@ $(document).ready(function() {
   whoseTurn();
 
   $('input[type="submit"]').on('click',function(){
-    div = $(this).closest("div")
-    cells = div.children('.cell')
-     for (i=5;i>-1;i--) {
-      console.log(cells[i])
-    }
+    turtle = $(this).closest("div")
+    cells = turtle.children('.cell')
 
+    var currentEmptyCell = []
+    for (i=5;i>-1;i--) {
+      // console.log(cells[i])
+      if (cells.eq(i).is(':empty')) {
+        // console.log(cells[i])
+        // console.log(cells.eq(i))
+        currentEmptyCell.push(i)
+      }
+    }
+    console.log(currentEmptyCell[0])
+    cells.eq(currentEmptyCell[0]).append('test')
   })
 
 
@@ -17,8 +25,8 @@ $(document).ready(function() {
 function turnCounter() {
   var counter = 0;
   $('form').on("submit", function(event){
-      event.preventDefault();
-      counter +=1;
+    event.preventDefault();
+    counter +=1;
   })
   return counter;
 }
@@ -39,3 +47,4 @@ function whoseTurn() {
 
 
 
+// console.log(cells[i])
