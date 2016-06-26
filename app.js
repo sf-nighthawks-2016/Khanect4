fca145ee3d4d232cb39f3ae7b2d528b2c4b1b35e
Chris Wong & Jonathan Hall & Mikey Moore <pair+chris-wong-1.jlhall.mikeymoore@devbootcamp.com>
2016-06-26 13:20:01 -0700

    Add alternating token logic for 2-player

--- app.js
+++ app.js
@@ -1,50 +1,36 @@
 $(document).ready(function() {
-  turnCounter();
-  whoseTurn();
+  var turn = "black"
+  function turnCounter() {
+    if (turn === "black") {
+      turn = "red"
+    }
+    else {
+      turn = "black"
+    }
+  }
 
-  $('input[type="submit"]').on('click',function(){
-    turtle = $(this).closest("div")
-    cells = turtle.children('.cell')
+  function whoseTurn() {
+    if (turn === "black") {
+      turnCounter();
+      return "<img alt='Ghengis Black Token' src='genghis_khan_B.png'>"
+    }
+    else {
+      turnCounter();
+      return "<img alt='Ghengis Red Token' src='genghis_khan_R.png'>"
+    }
+  }
 
-    var currentEmptyCell = []
+  $('input[type="submit"]').on('click',function(event){
+    event.preventDefault();
+    turtle = $(this).closest("div");
+    cells = turtle.children('.cell');
+
+    var currentEmptyCell = [];
     for (i=5;i>-1;i--) {
-      // console.log(cells[i])
       if (cells.eq(i).is(':empty')) {
-        // console.log(cells[i])
-        // console.log(cells.eq(i))
-        currentEmptyCell.push(i)
+        currentEmptyCell.push(i);
       }
     }
-    console.log(currentEmptyCell[0])
-    cells.eq(currentEmptyCell[0]).append('test')
+    cells.eq(currentEmptyCell[0]).append(whoseTurn());
   })
-
-
 });
-
-function turnCounter() {
-  var counter = 0;
-  $('form').on("submit", function(event){
-    event.preventDefault();
-    counter +=1;
-  })
-  return counter;
-}
-
-function whoseTurn() {
-  if (turnCounter() % 2 == 0) {
-    return "black";
-  }
-  else {
-    return "red";
-  }
-}
-
-
-
-
-// $(dfdk).prepend("<img alt='ghengis' src='kdjfk.jpg'>")
-
-
-
-// console.log(cells[i])
