c6d50af855ed4164ab4cbf88c2a0b0dbfbb361f7
Chris Wong & Jonathan Hall & Mikey Moore <pair+chris-wong-1.jlhall.mikeymoore@devbootcamp.com>
2016-06-26 10:54:12 -0700

    JS in progress

---
+++ app.js
@@ -0,0 +1,24 @@
+$(document).ready(function{
+  // turnCounter();
+  $('.board').on('submit', function(event){
+    event.preventDefault();
+    console.log("hello")
+  })
+
+
+});
+
+// function turnCounter() {
+//   var counter = 0;
+//   $('form').on("submit", function(event){
+//       event.preventDefault();
+//       var counter +=1;
+//       console.log(counter);
+//   })
+// }
+
+
+
+
+
+
